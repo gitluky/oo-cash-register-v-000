29ff48cf7f040940bcd9ee6b6487222fa1369396
Jonathan Luk <jluk87@gmail.com>
2018-03-15 23:07:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,60 +1,34 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount, :shopping_cart, :items
+  @@transactions = []
+
+  attr_accessor :total,:items,:discount,:current_transaction
 
   def initialize(discount = nil)
-    @discount = discount || 0
     @total = 0
-    @shopping_cart ||= {}
-    @items = []
+    @discount = discount
+    @current_transaction = {}
   end
 
   def total
     @total
   end
 
-  def discount
-    @discount
-  end
-
-  def items
-    @items
-  end
-
-  def add_item(title,price, quantity = 1)
-    i = 1
-    while i <= quantity
-      self.items << title
-      i += 1
+  def add_item(title, price, quantity = 1)
+    current_transaction[title] ||= {}
+    current_transaction[title][price] += 1 || = quantity
+    @total = current_transaction.collect do |title, prices|
+      prices.collect {|price, quantity| price * quantity}
     end
-    self.shopping_cart[title] ||= {} #creates hash if non-existent
-    self.shopping_cart[title][price] = quantity #adds a pricepoint and quantity for the item at that pricepoint
-    self.total = self.shopping_cart.collect {|title, prices| prices.keys[0] * prices.values[0]}.inject(:+) || 0#creates an array of price * quantity for each item and sums them, then updates @total
-    self.shopping_cart
+    @total = new_total.flatten.inject(:+) || 0
   end
 
   def apply_discount
-    if self.discount > 0
-      self.total -= self.total * (self.discount * 0.01)
-      "After the discount, the total comes to $#{self.total.to_i}."
-    else
-      "There is no discount to apply."
-    end
+
   end
 
-  def void_last_transaction
-    self.shopping_cart.clear
-    self.total = 0
+  def items
+
   end
-
-
 end
-
-cash_register = CashRegister.new(20)
-cash_register.add_item("macbook air", 1000)
-cash_register.apply_discount
-puts cash_register.items
-cash_register.void_last_transaction
-puts cash_register.shopping_cart
-puts cash_register.total
