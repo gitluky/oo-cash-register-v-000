c43324d930a8f977ebaf3a7829f7bfa644973fd1
Jonathan Luk <jluk87@gmail.com>
2018-03-16 01:35:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -48,7 +48,7 @@ class CashRegister
 
   def void_last_transaction
     puts "current_transaction was #{current_transaction}"
-    self.current_transaction = self.current_transaction[@@transactions[-1][0]][@@transactions[-1][1]] - [@@transactions[-1][2]]
+    self.current_transaction = self.current_transaction[@@transactions[-1][0]][@@transactions[-1][1]].to_i - [@@transactions[-1][2]]
     puts "the updated transaction is #{current_transaction}"
   end
 end
